216315b9e7b054c1fd1458c005e49fab34deed47
bdhom <bjdhom@gmail.com>
2022-12-19 21:17:15 -0600

    Moved io mappings to own header

---
+++ Core/Inc/stm32f4xx_ioMapping.h
@@ -0,0 +1,27 @@
+#ifndef __STM32F4XX_IOMAPPING_H
+#define __STM32F4XX_IOMAPPING_H
+
+#define B1_Pin GPIO_PIN_0
+#define B1_GPIO_Port GPIOA
+
+#define OLED_CS_Pin GPIO_PIN_0
+#define OLED_CS_Port GPIOB
+#define OLED_DC_Pin GPIO_PIN_1
+#define OLED_DC_Port GPIOB
+#define OLED_RES_Pin GPIO_PIN_2
+#define OLED_RES_Port GPIOB
+#define OLED_SCLK_Pin GPIO_PIN_3
+#define OLED_SCLK_Port GPIOB
+#define OLED_SDIN_Pin GPIO_PIN_5
+#define OLED_SDIN_Port GPIOB
+#define OLED_Port GPIOB
+
+#define LD4_Pin GPIO_PIN_12
+#define LD4_GPIO_Port GPIOD
+#define LD3_Pin GPIO_PIN_13
+#define LD3_GPIO_Port GPIOD
+#define LD5_Pin GPIO_PIN_14
+#define LD5_GPIO_Port GPIOD
+#define LD6_Pin GPIO_PIN_15
+
+#endif /* __STM32F4XX_IOMAPPING_H */
